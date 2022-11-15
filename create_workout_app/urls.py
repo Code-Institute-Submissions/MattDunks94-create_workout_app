5fa7057408e13a8c5e9b23fa914372466eee79d8
Matt <mjd014@live.co.uk>
2022-11-15 01:01:11 +0000

    added path to exercise_detail url

--- create_workout_app/urls.py
+++ create_workout_app/urls.py
@@ -4,4 +4,5 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', views.ExerciseList.as_view(), name='home'),
+    path('<int:pk>/', views.ExerciseDetail.as_view(), name='exercise_detail'),
 ]
