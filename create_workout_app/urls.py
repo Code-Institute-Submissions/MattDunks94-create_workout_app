c8a1671d9b755e697d792997e8be43fec13ff5c2
Matt <mjd014@live.co.uk>
2022-11-15 10:09:36 +0000

    added login required to home url.

--- create_workout_app/urls.py
+++ create_workout_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('', views.ExerciseList.as_view(), name='home'),
+    path('', login_required(views.ExerciseList.as_view()), name='home'),
     path('<int:pk>/', views.ExerciseDetail.as_view(), name='exercise_detail'),
     path('create_exercise/', views.CreateExercise.as_view(), name='create_exercise'),
     path('create_workout/', views.CreateWorkoutCategory.as_view(), name='create_workout'),
