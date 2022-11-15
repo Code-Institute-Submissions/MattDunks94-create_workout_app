ec5e16b82e62afd94b9a94d209da0883b6c8b931
Matt <mjd014@live.co.uk>
2022-11-15 01:34:05 +0000

    added create view for Exercise model, for creating new exercises.

--- create_workout_app/views.py
+++ create_workout_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 from .models import WorkoutCategory, Exercise
+from .forms import CreateExerciseForm
 
 
 class ExerciseList(generic.ListView):
@@ -12,3 +13,16 @@ class ExerciseList(generic.ListView):
 class ExerciseDetail(generic.DetailView):
     model = Exercise
     template_name = 'exercise_detail.html'
+
+
+class CreateExercise(generic.CreateView):
+    model = Exercise
+    form_class = CreateExerciseForm
+    template_name = 'create_exercise.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
+
