de78e40a34d49af20c44c71c0d9273663f2379a5
Matt <mjd014@live.co.uk>
2022-11-15 14:27:38 +0000

    added success messages to all createview views.

--- create_workout_app/views.py
+++ create_workout_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
-from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from .models import WorkoutCategory, Exercise
 from .forms import CreateExerciseForm, CreateWorkoutCategoryForm, EditExerciseForm, EditWorkoutCategoryForm
 
@@ -16,43 +16,48 @@ class ExerciseDetail(generic.DetailView):
     template_name = 'exercise_detail.html'
 
 
-class CreateExercise(generic.CreateView):
+class CreateExercise(SuccessMessageMixin, generic.CreateView):
     model = Exercise
     form_class = CreateExerciseForm
     template_name = 'create_exercise.html'
     success_url = reverse_lazy('home')
+    success_message = 'Successfully created %(exercise)s!'
 
     def form_valid(self, form):
         form.instance.created_by = self.request.user
         return super().form_valid(form)
 
 
-class CreateWorkoutCategory(generic.CreateView):
+class CreateWorkoutCategory(SuccessMessageMixin, generic.CreateView):
     model = WorkoutCategory
     form_class = CreateWorkoutCategoryForm
     template_name = 'create_workout.html'
     success_url = reverse_lazy('home')
+    success_message = 'Successfully created %(title)s!'
 
     def form_valid(self, form):
         form.instance.published_by = self.request.user
         return super().form_valid(form)
 
 
-class EditExercise(generic.UpdateView):
+class EditExercise(SuccessMessageMixin, generic.UpdateView):
     model = Exercise
     form_class = EditExerciseForm
     template_name = 'edit_exercise.html'
     success_url = reverse_lazy('home')
+    success_message = 'Successfully edited %(exercise)s!'
 
 
-class EditWorkoutCategory(generic.UpdateView):
+class EditWorkoutCategory(SuccessMessageMixin, generic.UpdateView):
     model = WorkoutCategory
     form_class = EditWorkoutCategoryForm
     template_name = 'edit_workout.html'
     success_url = reverse_lazy('home')
+    success_message = 'Successfully edited %(title)s!'
 
 
 class DeleteExercise(generic.DeleteView):
     model = Exercise
     template_name = 'delete_exercise.html'
     success_url = reverse_lazy('home')
+
