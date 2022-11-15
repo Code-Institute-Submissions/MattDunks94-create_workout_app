5b4c62780af1088a2fb1c10ad5460ac13f714c64
Matt <mjd014@live.co.uk>
2022-11-15 02:09:03 +0000

    added path for edit_exercise url.

--- create_workout_app/urls.py
+++ create_workout_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('<int:pk>/', views.ExerciseDetail.as_view(), name='exercise_detail'),
     path('create_exercise/', views.CreateExercise.as_view(), name='create_exercise'),
     path('create_workout/', views.CreateWorkoutCategory.as_view(), name='create_workout'),
+    path('edit_exercise/<int:pk>/', views.EditExercise.as_view(), name='edit_exercise'),
 ]
