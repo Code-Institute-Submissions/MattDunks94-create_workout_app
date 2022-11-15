3f0e5a4295956a69d92c9074e771bdd3d8a3380f
Matt <mjd014@live.co.uk>
2022-11-15 17:37:37 +0000

    created a series of tests for our views.

--- create_workout_app/tests_views.py
+++ create_workout_app/tests_views.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    def test_get_index(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'templates/index.html')
+
+    def test_get_create_exercise(self):
+        response = self.client.get('/create_exercise')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'templates/create_exercise.html')
+
+    def test_get_create_workout(self):
+        response = self.client.get('/create_workout')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'templates/create_workout.html')
+
+    def test_get_exercise_detail(self):
+        response = self.client.get('/<int:pk>')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'templates/exercise_detail.html')
+
+    def test_get_edit_exercise(self):
+        response = self.client.get('/edit_exercise/4')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'templates/edit_exercise.html')
+
+    def test_get_delete_exercise(self):
+        response = self.client.get('/delete_exercise/4')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'templates/delete_exercise.html')
+
+    def test_can_create_exercise(self):
+        response = self.client.post('/create_exercise')
+        self.assertRedirects(response, '/')
+    
+
