45e8b7cd6c96c0196e70667240b076ee69b691d0
Matt <mjd014@live.co.uk>
2022-11-15 17:46:46 +0000

    added a test for our model WorkoutCategory

--- create_workout_app/tests_models.py
+++ create_workout_app/tests_models.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import WorkoutCategory
 
-# Create your tests here.
+
+class TestModels(TestCase):
+    
+    def test_featured_image_default(self):
+        workout = WorkoutCategory.objects.create(title='Test Workout')
+        self.assertContains(workout.featured_image.default)
