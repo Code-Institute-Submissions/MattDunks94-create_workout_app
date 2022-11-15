5a9bf449d35c3e152dfd54808abfd10af4d42a8c
Matt <mjd014@live.co.uk>
2022-11-15 17:37:10 +0000

    added two tests testing field requireme

--- create_workout_app/tests_forms.py
+++ create_workout_app/tests_forms.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .forms import CreateExerciseForm
 
-# Create your tests here.
+
+class TestCreateExerciseForm(TestCase):
+
+    def test_exercise_name_required(self):
+        form = CreateExerciseForm({'exercise': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('exercise', form.errors.keys())
+        self.assertEqual(form.errors['exercise'][0], 'This field is required.')
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = CreateExerciseForm()
+        self.assertEqual(form.Meta.fields, ['exercise', 'sets', 'reps', 'weight', 'workout'])
