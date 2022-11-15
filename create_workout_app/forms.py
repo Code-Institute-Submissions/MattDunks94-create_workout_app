804fee61188fab6a037a4b3d88240ffdd53a95a2
Matt <mjd014@live.co.uk>
2022-11-15 01:54:05 +0000

    added form to create workout categories.

--- create_workout_app/forms.py
+++ create_workout_app/forms.py
@@ -6,3 +6,9 @@ class CreateExerciseForm(forms.ModelForm):
     class Meta:
         model = Exercise
         fields = ['exercise', 'sets', 'reps', 'weight', 'workout']
+
+
+class CreateWorkoutCategoryForm(forms.ModelForm):
+    class Meta:
+        model = WorkoutCategory
+        fields = ['title', 'featured_image']
