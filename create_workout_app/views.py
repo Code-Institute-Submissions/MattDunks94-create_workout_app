73bf7a67583a7baabbe617a34f82b90005ebd5e1
Matt <mjd014@live.co.uk>
2022-11-15 01:02:03 +0000

    added detail view for exercise model.

--- create_workout_app/views.py
+++ create_workout_app/views.py
@@ -7,3 +7,8 @@ from .models import WorkoutCategory, Exercise
 class ExerciseList(generic.ListView):
     model = Exercise
     template_name = 'index.html'
+
+
+class ExerciseDetail(generic.DetailView):
+    model = Exercise
+    template_name = 'exercise_detail.html'
