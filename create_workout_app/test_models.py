e0f3d46857b14b6b5feeeeb44acc8072427a733c
Matt <mjd014@live.co.uk>
2022-11-27 17:20:29 +0000

    added test, testing workout category model image default.

--- create_workout_app/test_models.py
+++ create_workout_app/test_models.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
+from .models import WorkoutCategory
 
 
-class TestDjango(TestCase):
+class TestModels(TestCase):
 
-    def test_this_thing(self):
-        self.assertEqual(1, 0)
+    def test_featured_image_placeholder_as_default(self):
+        workout = WorkoutCategory.objects.create(title='Test Workout')
+        self.assertEqual(workout.featured_image, 'placeholder')
